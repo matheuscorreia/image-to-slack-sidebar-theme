f2d3f67654d858447571011e113e48f998629a92
Matheus Correia <matheuscorreia2005@gmail.com>
2019-08-01 14:34:57 -0300

    add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as serviceWorker from './serviceWorker';
+
 import App from './App';
+
+serviceWorker.register();
 
 ReactDOM.render(<App />, document.getElementById('root'));
